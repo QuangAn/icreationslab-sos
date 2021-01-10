f13e2a7a12aae20e061783b22ed39c34d5a8558f
Quang An <an.tin3a1@gmail.com>
2021-01-10 16:33:51 +0700

    update

--- wp-content/themes/sos/footer.php
+++ wp-content/themes/sos/footer.php
@@ -42,19 +42,5 @@
 
 <?php wp_footer(); ?>
 <div id="back_top"></div>
-<script>
-  jQuery(document).ready(function(){
-    jQuery(window).scroll(function() {
-        if (jQuery(this).scrollTop() > 300) {
-            jQuery('#back_top').fadeIn();
-        } else {
-            jQuery('#back_top').fadeOut();
-        }
-    });
-    jQuery('#back_top').click(function() {
-      jQuery('body,html').animate({ scrollTop: 0 }, 1000);
-    });
-  });
-</script>
 </body>
 </html>
--- wp-content/themes/sos/js/custom.js
+++ wp-content/themes/sos/js/custom.js
@@ -142,4 +142,15 @@ jQuery(document).ready(function() {
             scrollTop: jQuery(".quiz").offset().top
         }, 500);
     });
+
+    jQuery(window).scroll(function() {
+        if (jQuery(this).scrollTop() > 300) {
+            jQuery('#back_top').fadeIn();
+        } else {
+            jQuery('#back_top').fadeOut();
+        }
+    });
+    jQuery('#back_top').click(function() {
+        jQuery('body,html').animate({ scrollTop: 0 }, 1000);
+    });
 });
