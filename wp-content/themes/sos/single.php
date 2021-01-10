10fa4c68a43bb59b015bd7081f1109d6f7c8c91e
Quang An <an.tin3a1@gmail.com>
2021-01-10 11:26:15 +0700

    update

--- wp-content/themes/sos/single.php
+++ wp-content/themes/sos/single.php
@@ -8,8 +8,28 @@
  */
 
 get_header();
-?>
 
+?>
+<?php 
+		global $post;
+		$categories = get_the_category($post->ID);
+		$cat_id = $categories[0]->cat_ID;
+		$description = $categories[0]->description;
+		$name = $categories[0]->name;
+		$image = get_field('feature_image', 'category_'. $cat_id);
+		$show_sidebar = get_field('show_sidebar');
+	?>
+<div class="page-header">
+	<div class="feature_image_category">
+		<img src="<?php echo $image['url']; ?>" alt="" />
+	</div>
+	<div class="box-des-category">
+		<h1 class="page-title"><?=  $name;  ?></h1>
+		<div class="archive-description"><?= $description ?></div>
+	</div>
+</div>	
+<div class="sigle-main-content">
+	
 	<main id="primary" class="site-main">
 
 		<?php
@@ -18,23 +38,16 @@ get_header();
 
 			get_template_part( 'template-parts/content', get_post_type() );
 
-			the_post_navigation(
-				array(
-					'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'sos' ) . '</span> <span class="nav-title">%title</span>',
-					'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'sos' ) . '</span> <span class="nav-title">%title</span>',
-				)
-			);
 
 			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+			// if ( comments_open() || get_comments_number() ) :
+			// 	comments_template();
+			// endif;
 
 		endwhile; // End of the loop.
 		?>
-
 	</main><!-- #main -->
-
+	<?php $show_sidebar == 'yes' ? get_sidebar() : ''; ?>
+</div>
 <?php
-get_sidebar();
 get_footer();
--- wp-content/themes/sos/template-parts/content.php
+++ wp-content/themes/sos/template-parts/content.php
@@ -10,12 +10,13 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<div class="entry-thumbnail">
-		<a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>"  class="thumbnail__img"><?php the_post_thumbnail('thumbnail'); ?></a>
-	</div>
+	
 	<div class="entry-header">
 		<?php the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' ); ?>
 	</div><!-- .entry-header -->
 	<div class="entry-meta"><?php sos_posted_on(); ?></div>
+	<div class="entry-thumbnail">
+		<a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>"  class="thumbnail__img"><?php the_post_thumbnail('full'); ?></a>
+	</div>
 	<div class="entry-content"><?php echo wp_strip_all_tags(get_the_excerpt()); ?></div>
 </article><!-- #post-<?php the_ID(); ?> -->
