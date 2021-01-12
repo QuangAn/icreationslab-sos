da1de2b9c9e94570850a862eb1482caff232c40a
Quang An <an.tin3a1@gmail.com>
2021-01-12 14:01:10 +0700

    update

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'wp_sos' );
+define( 'DB_NAME', 'soseaorg_data' );
 
 /** MySQL database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'soseaorg_user' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'elyB$.1Uv2TM' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
